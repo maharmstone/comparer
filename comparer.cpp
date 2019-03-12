b3b58322106d7b419bc44ddaab5bbcb6feb3ffa8
Mark Harmstone <mark.harmstone@boltonft.nhs.uk>
2019-03-12 16:40:10 +0000

    log to table

--- comparer.cpp
+++ comparer.cpp
@@ -38,6 +38,7 @@ void db_connect() {
 static void do_compare(unsigned int num) {
 	string q1, q2;
 	bool b1, b2;
+	unsigned int rows1 = 0, rows2 = 0, changed_rows = 0, added_rows = 0, removed_rows = 0;
 
 	{
 		SQL(sq, "SELECT query1, query2 FROM Comparer.queries WHERE id=?", num);
@@ -64,16 +65,26 @@ static void do_compare(unsigned int num) {
 			string pk1 = sq1.cols[0], pk2 = sq2.cols[0];
 
 			if (pk1 == pk2) {
+				bool changed = false;
+
 				for (unsigned int i = 1; i < sq1.cols.size(); i++) {
 					if (sq1.cols[i].null && !sq2.cols[i].null) {
 						run_sql("INSERT INTO Comparer.results(query, primary_key, change, col, value1, value2) VALUES(?, ?, ?, ?, NULL, ?)", num, pk1, "modified", i + 1, (string)sq2.cols[i]);
+						changed = true;
 					} else if (!sq1.cols[i].null && sq2.cols[i].null) {
 						run_sql("INSERT INTO Comparer.results(query, primary_key, change, col, value1, value2) VALUES(?, ?, ?, ?, ?, NULL)", num, pk1, "modified", i + 1, (string)sq1.cols[i]);
+						changed = true;
 					} else if ((string)sq1.cols[i] != (string)sq2.cols[i]) {
 						run_sql("INSERT INTO Comparer.results(query, primary_key, change, col, value1, value2) VALUES(?, ?, ?, ?, ?, ?)", num, pk1, "modified", i + 1, (string)sq1.cols[i], (string)sq2.cols[i]);
+						changed = true;
 					}
 				}
 
+				if (changed)
+					changed_rows++;
+
+				rows1++;
+				rows2++;
 				b1 = sq1.fetch_row();
 				b2 = sq2.fetch_row();
 			} else if (pk1 < pk2) {
@@ -85,6 +96,8 @@ static void do_compare(unsigned int num) {
 					}
 				}
 
+				removed_rows++;
+				rows1++;
 				b1 = sq1.fetch_row();
 			} else {
 				for (unsigned int i = 1; i < sq2.cols.size(); i++) {
@@ -95,6 +108,8 @@ static void do_compare(unsigned int num) {
 					}
 				}
 
+				added_rows++;
+				rows2++;
 				b2 = sq2.fetch_row();
 			}
 		} else if (b1) {
@@ -108,6 +123,8 @@ static void do_compare(unsigned int num) {
 				}
 			}
 
+			removed_rows++;
+			rows1++;
 			b1 = sq1.fetch_row();
 		} else {
 			string pk2 = sq2.cols[0];
@@ -120,18 +137,26 @@ static void do_compare(unsigned int num) {
 				}
 			}
 
+			added_rows++;
+			rows2++;
 			b2 = sq2.fetch_row();
 		}
 	}
+
+	run_sql("INSERT INTO Comparer.log(query, success, rows1, rows2, changed_rows, added_rows, removed_rows) VALUES(?, 1, ?, ?, ?, ?, ?)", num, rows1, rows2, changed_rows, added_rows, removed_rows);
 }
 
 int main() {
-	try {
-		db_connect();
+	unsigned int num = 1; // FIXME
 
-		do_compare(1);
+	db_connect();
+
+	try {
+		do_compare(num);
 	} catch (const exception& e) {
 		cerr << "Exception: " << e.what() << endl;
+		run_sql("INSERT INTO Comparer.log(query, success, error) VALUES(?, 0, ?)", num, e.what());
+
 		return 1;
 	}
 
